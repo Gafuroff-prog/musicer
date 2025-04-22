2260d016604ff75da57c0a7898f37df655ff2d74
Gafuroff-prog <gafuroffgaming@gmail.com>
2025-04-22 19:09:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let audio = document.getElementById("audio");
+let playButton = document.getElementById("play");
+let pauseButton = document.getElementById("pause");
+
+function playAudio() {
+    audio.play();
+}
+
+function pauseAudio() {
+    audio.pause();
+}   
